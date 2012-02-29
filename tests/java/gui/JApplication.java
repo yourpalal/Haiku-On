2896c403a614689237d52e1ed99174ae828adfcf
Alex Wilson <yourpalal2@gmail.com>
2012-02-29 18:23:12 +1300

    Improve the GUI test a bit.

--- tests/java/gui/JApplication.java
+++ tests/java/gui/JApplication.java
@@ -1,4 +1,5 @@
 import org.haiku.*;
+import static org.haiku.haikuConstants.*;
 
 
 class JApplication extends BApplication {
@@ -12,10 +13,21 @@ class JApplication extends BApplication {
 		super("application/x-vnd.Haiku-onJava-GUItest");
 
 		fWindow = new BWindow(new BRect(0, 0, 200, 200), "JAVA HAIKU GUI!!!",
-			window_type.B_TITLED_WINDOW);
-		fWindow.SetLayout(new BGroupLayout(orientation.B_HORIZONTAL));
-		fWindow.AddChild(new BButton("JAAAAAAAAAAAVAAAAAA"));
+			window_type.B_TITLED_WINDOW, B_AUTO_UPDATE_SIZE_LIMITS);
+
+		fWindow.SetLayout(new BGroupLayout(orientation.B_VERTICAL, 0));
+
+		fWindow.AddChild(_makeUnlimited(new BButton("JAAAAAAAAAAAVAAAAAA")));
+		fWindow.AddChild(_makeUnlimited(
+			new BStringView("string view", "WOOOO HOOO")));
+
 		fWindow.Show();
+	}
+
+	private BView _makeUnlimited(BView view)
+	{
+		view.SetExplicitMaxSize(new BSize(B_SIZE_UNLIMITED, B_SIZE_UNLIMITED));
+		return view;
 	}
 
 	private BWindow fWindow;
